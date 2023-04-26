c8fee0e433bff3a8d9167948686b432106a20ce4
SushantXarpie <119557915+SushantXarpie@users.noreply.github.com>
2023-04-26 18:12:20 +0530

    Setup the Controller

---
+++ Assets/Script/Controller.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Game game;
+    // public UIController uiController; 
+
+    public void Init()
+    {
+        game.InitializeGame();
+        UpdateUI();
+    }
+
+    public void HandleAnswer(string answer)
+    {
+        if (game.IsAnswerCorrect(answer))
+        {
+            game.HandleCorrectAnswer();
+        }
+        else
+        {
+            game.HandleWrongAnswer();
+        }
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+
+    }
+}
